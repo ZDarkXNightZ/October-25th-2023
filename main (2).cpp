c07f5bfe7d659d5935db04c569bf7a47889474fa
Jeevan Singh <147223417+ZDarkXNightZ@users.noreply.github.com>
2023-10-25 14:39:05 -0400

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string secret_password = "acer";
+    string user_guess;
+     bool isCorrect = true;
+
+    cout<<"Enter secret password"<<endl;
+    cin>>user_guess;
+
+    if(user_guess==secret_password)
+    {
+     cout << "You got it!" << endl;
+    }
+    else {
+        cout << "Try again." << endl;
+    }
+
+}
